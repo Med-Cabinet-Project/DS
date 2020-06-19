75ab429439487b503fe468ec95054a4dc5033fe6
andrewwhite5 <andrewwhite5@users.noreply.github.com>
2020-06-18 20:33:49 -0600

    Adding .gitignore

--- src/strain_api.py
+++ src/strain_api.py
@@ -1,23 +1,23 @@
-# import requests
-#
-# # URLs
-# base_url = requests.get('strainapi.evanbusse.com/API_KEY')
-# effects_url = requests.get('strainapi.evanbusse.com/API_KEY/searchdata/effects')
-# flavors_url = requests.get('strainapi.evanbusse.com/API_KEY/searchdata/flavors')
-#
-# # Get all strains (Use sparingly -- requires a lot of computing power)
-# strains_url = requests.get('strainapi.evanbusse.com/API_KEY/strains/search/all')
-#
-# # Search for strains by name
-# requests.get('strainapi.evanbusse.com/API_KEY/strains/search/name/NAME')
-#
-# # Search for strains by race (Available races: Sativa, Indica, and Hybrid)
-# strainapi.evanbusse.com/API_KEY/strains/search/race/RACE
-# # Search for strains by effect
-# strainapi.evanbusse.com/API_KEY/strains/search/effect/EFFECT
-# # Search for strains by flavor
-# strainapi.evanbusse.com/API_KEY/strains/search/flavor/FLAVOR
-#
-# response = requests.get("strainapi.evanbusse.com/API_KEY/searchdata/effects
-# ")
-# print(response.status_code)
+import requests
+
+# URLs
+base_url = requests.get('strainapi.evanbusse.com/API_KEY')
+effects_url = requests.get('strainapi.evanbusse.com/API_KEY/searchdata/effects')
+flavors_url = requests.get('strainapi.evanbusse.com/API_KEY/searchdata/flavors')
+
+# Get all strains (Use sparingly -- requires a lot of computing power)
+strains_url = requests.get('strainapi.evanbusse.com/API_KEY/strains/search/all')
+
+# Search for strains by name
+requests.get('strainapi.evanbusse.com/API_KEY/strains/search/name/NAME')
+
+# Search for strains by race (Available races: Sativa, Indica, and Hybrid)
+strainapi.evanbusse.com/API_KEY/strains/search/race/RACE
+# Search for strains by effect
+strainapi.evanbusse.com/API_KEY/strains/search/effect/EFFECT
+# Search for strains by flavor
+strainapi.evanbusse.com/API_KEY/strains/search/flavor/FLAVOR
+
+response = requests.get("strainapi.evanbusse.com/API_KEY/searchdata/effects
+")
+print(response.status_code)

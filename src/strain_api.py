5e1f1d1557b539adeba53f5710edf035f697c5d0
andrewwhite5 <andrewwhite5@users.noreply.github.com>
2020-06-18 20:54:21 -0600

    Env variables

--- src/strain_api.py
+++ src/strain_api.py
@@ -8,16 +8,21 @@ flavors_url = requests.get('strainapi.evanbusse.com/API_KEY/searchdata/flavors')
 # Get all strains (Use sparingly -- requires a lot of computing power)
 strains_url = requests.get('strainapi.evanbusse.com/API_KEY/strains/search/all')
 
-# Search for strains by name
-requests.get('strainapi.evanbusse.com/API_KEY/strains/search/name/NAME')
+# # Search for strains by name
+# requests.get('strainapi.evanbusse.com/API_KEY/strains/search/name/NAME')
+#
+# # Search for strains by race (Available races: Sativa, Indica, and Hybrid)
+# strainapi.evanbusse.com/API_KEY/strains/search/race/RACE
+# # Search for strains by effect
+# strainapi.evanbusse.com/API_KEY/strains/search/effect/EFFECT
+# # Search for strains by flavor
+# strainapi.evanbusse.com/API_KEY/strains/search/flavor/FLAVOR
+#
+# response = requests.get("strainapi.evanbusse.com/API_KEY/searchdata/effects
+# ")
+# print(response.status_code)
 
-# Search for strains by race (Available races: Sativa, Indica, and Hybrid)
-strainapi.evanbusse.com/API_KEY/strains/search/race/RACE
-# Search for strains by effect
-strainapi.evanbusse.com/API_KEY/strains/search/effect/EFFECT
-# Search for strains by flavor
-strainapi.evanbusse.com/API_KEY/strains/search/flavor/FLAVOR
-
-response = requests.get("strainapi.evanbusse.com/API_KEY/searchdata/effects
-")
-print(response.status_code)
+URL = BASE_URL + API_KEY + ENDPOINT
+r = requests.get(url=URL)
+data = r.json()
+print(data)

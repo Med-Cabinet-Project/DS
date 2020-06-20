5c8c1aab4dc054017ae0ce8305c0236e8f1cdb14
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-20 13:32:08 -0400

    Creating other files  to expediate Flask creation

---
+++ web_app/routes/strains_routes.py
@@ -0,0 +1,33 @@
+
+from flask import Blueprint, render_template, flash, redirect
+import requests
+from web_app.models import db, Strain, get_records, add_records, parse_records
+from web_app.services.strains_service import api
+
+aq_routes = Blueprint("aq_routes", __name__)
+
+@aq_routes.route('/home', methods=["GET", "POST"])
+def root():
+    """Base view."""
+    #`Record` objects that have `value` greater or equal to 10.
+    add_records(get_records())
+
+    records = Record.query.filter(Record.value >= 10).all()
+    print(records)
+
+    return render_template("form.html", records=records, message="Home Page")
+
+@aq_routes.route('/refresh')
+def refresh():
+    """Replace existing data with data from OpenAQ."""
+    DB.drop_all()
+    DB.create_all()
+
+    # add new records
+    add_records(get_records())
+    records = Record.query.filter(Record.value >=10).all()
+    
+    print(records)
+
+    flash(f"'Data refreshed!", "success") # "danger" "warning"
+    return render_template("refresh.html", records=records)

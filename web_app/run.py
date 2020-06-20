5a2f19a7b4de3fdb24255dd81419742fb9e2a343
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-19 22:10:12 -0400

    Exploring data and seeing how to extract the information based on dict keys

--- web_app/run.py
+++ web_app/run.py
@@ -1,8 +1,11 @@
 
 import os
 import requests
+import urllib.request as request
+import json
 from pprint import pprint 
 from dotenv import load_dotenv
+import numpy as np 
 
 load_dotenv()
 
@@ -11,8 +14,20 @@ BASE_URL = "http://strainapi.evanbusse.com/"
 ENDPOINT = "/strains/search/all"
 URL = BASE_URL + API_KEY + ENDPOINT 
 
+#Get all the strains and information about effects
 r = requests.get(url=URL)
-
 data = r.json()
-
 pprint(data)
+
+#Exploring data and seeing how to extract specific values
+with request.urlopen(URL) as response:
+    if response.getcode() == 200:
+            source = response.read()
+            data = json.loads(source)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
+pprint(type(data))
+print(len(data.keys()))
+# pprint(data.keys())   
--- web_app/strain_api.py
+++ web_app/strain_api.py
@@ -1,6 +1,6 @@
-import os
 import requests
-import pprint as pprint
+import os
+from pprint import pprint 
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,9 +11,9 @@ API_1_KEY = os.getenv('API_1_KEY', default='Oops')
 # URLs
 EFFECTS_ENDPOINT = '/searchdata/effects'
 BASE_1_URL = 'http://strainapi.evanbusse.com/'
+
+
 URL = BASE_1_URL + API_1_KEY + EFFECTS_ENDPOINT
-
-
 r = requests.get(url=URL)
 data = r.json()
 pprint(data)

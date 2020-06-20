3dd9b0a7ca795f26f412372ab8bb3b7177cc802f
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-20 11:08:07 -0400

    Creating a models.py page to connect for postgres table

--- web_app/models.py
+++ web_app/models.py
@@ -9,37 +9,14 @@ migrate = Migrate()
 
 class Strain(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    strain = db.Column(db.String(128))
-
-    def __repr__(self):
-        return f"<Strain {self.id} {self.strain}>"
-
-
-class Medical(db.Model):
-    id = db.Column(db.BigInteger, primary_key = True)
+    name = db.Column(db.String(128))
     medical_needs = db.Column(db.String(128), nullable = False)
-
-    def __repr__(self):
-        return f"<Medical {self.id} {self.medical_needs}>"
-
-class Effects(db.Model):
-    id = db.Column(db.BigInteger, primary_key = True)
     effects_positive = db.Column(db.String(128), nullable = False)
     effects_negative = db.Column(db.String(128), nullable = True)
-
-    def __repr__(self):
-        return f"<Positive Effects {self.id} {self.effects_positive} {self.effects_negative}>"
-
-class Flavors(db.Model):
-    id = db.Column(db.BigInteger, primary_key = True)
-    tastes_like = db.Column(db.String(128), nullable = False)
-
-    def __repr__(self):
-        return f"<Tastes Like {self.id} {self.tastes_like}>"
-
-class Race(db.Model):
-    id = db.Column(db.BigInteger, primary_key = True)
+    flavor = db.Column(db.String(128), nullable = False)
     race = db.Column(db.String(128), nullable = False)
 
     def __repr__(self):
-        return f"<Race {self.id} {self.race}>"
+        return f"<Strain {self.id} {self.name}{self.medical_needs}{self.effects_positive} {self.effects_negative}{self.flavor} >"
+
+
--- web_app/run.py
+++ web_app/run.py
@@ -1,11 +1,9 @@
 
 import os
-import requests
 import urllib.request as request
 import json
 from pprint import pprint 
 from dotenv import load_dotenv
-import numpy as np 
 
 load_dotenv()
 
@@ -13,11 +11,6 @@ API_KEY = os.getenv("API_KEY")
 BASE_URL = "http://strainapi.evanbusse.com/"
 ENDPOINT = "/strains/search/all"
 URL = BASE_URL + API_KEY + ENDPOINT 
-
-#Get all the strains and information about effects
-r = requests.get(url=URL)
-data = r.json()
-# pprint(data)
 
 #Exploring data and seeing how to extract specific values
 with request.urlopen(URL) as response:
@@ -27,13 +20,8 @@ with request.urlopen(URL) as response:
     else:
         print('An error occurred while attempting to retrieve data from the API.')
 
-# pprint(type(data))
+pprint(type(data))
 print(len(data.keys())) # 1970 different strains will not be possible to call each one separately
-
-# print("---DATA ITEMS----")
-for (k, v) in data.items():
-    print("Key: " + k)
-    print("Value: " + str(v))
 
 print("---STRAND----")
 
@@ -46,8 +34,6 @@ for key, value in data.items():
     strands_dict['positive'].append(value['effects']['positive'])
     strands_dict['negative'].append(value['effects']['negative'])
     strands_dict['flavors'].append(value['flavors'])
-
-
 
 pprint(strands_dict)
 

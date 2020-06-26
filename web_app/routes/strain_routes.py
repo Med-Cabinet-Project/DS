7855bb4b5e31c9ddb7f212b9670beab81f61c5a7
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-26 01:00:33 -0400

    Add import necessary packages and files for strain_routes

--- web_app/routes/strain_routes.py
+++ web_app/routes/strain_routes.py
@@ -4,6 +4,9 @@ from flask import Blueprint, request, render_template, flash, redirect, jsonify
 import os
 import pickle
 import json
+from sqlalchemy.sql.expression import func
+from web_app.models import DB, Strain, extract_data, create_table, parse_records
+from web_app.services.strains_service import API 
 
 strain_routes = Blueprint("strain_routes", __name__)
 
@@ -17,60 +20,45 @@ def before_request():
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add("Access-Control-Allow-Headers", "*")
         response.headers.add("Access-Control-Allow-Methods", "*")
-
         return response
 
     if request.method == "OPTIONS":
-        
         return _build_cors_prelight_response()
 
 @strain_routes.after_request
 def after_request(response):
     """ CORS headers, required for off-server access """
-
     header = response.headers
     header['Access-Control-Allow-Origin'] = '*'
     return response
 
 @strain_routes.route("/", methods=['GET', 'POST'])
 def root():
-    """
-        Return first 100 strains in pickle dict
-    """
-
+    """Return all strains in pickle dict"""
     PICKLE2_FILEPATH = os.path.join(os.path.dirname(__file__),"..", "stats_model", "strains_num.pkl")
     pickle2_dict = pickle.load(open(PICKLE2_FILEPATH, "rb"))
-
     return jsonify(pickle2_dict)
 
 @strain_routes.route("/<strain>", methods=['GET'])
 def get_strain(strain):
-    """
-        Can query pickled strains dictionary to get information about a particular strain
-    """
-
+    """ Can query pickled strains dictionary to get information about a particular strain"""
     PICKLE_FILEPATH = os.path.join(os.path.dirname(__file__),"..", "stats_model", "strains.pkl")
     pickle_dict = pickle.load(open(PICKLE_FILEPATH, "rb"))
-
     return jsonify({"strain":pickle_dict[strain]})
 
 @strain_routes.route('/data', methods=["GET", "POST"])
 def data():
-
     """View all strains in database."""
-
     DB.drop_all()
     DB.create_all()
     create_table(extract_data())
     strain = Strain.query.all()
     records = parse_records(strain)
-    
     return render_template("data.html", records=records, message="Home Page")
 
 #More query routes 
 @strain_routes.route('/types/<race>', methods=['GET'])
 def get_type(race):
-
     """User can query Strains DB based on type/race preference - sativa, indica, hybrid"""
 
     records = Strain.query.filter_by(race=race).order_by(func.random()).limit(5).all()
@@ -87,7 +75,6 @@ def get_type(race):
             "flavor": typ.flavors,
             "negative": typ.negative
         })
-
     return jsonify(types)
 
 @strain_routes.route('/medical/<medical>', methods=['GET'])
@@ -95,8 +82,8 @@ def get_medical(medical):
 
     """User can query Strains DB based on medical symptoms/ailmens - depression, nausea, etc."""
 
-    records = Strain.query.filter(Strain.medical.ilike(f"%{medical}%", escape="/")).order_by(func.random()).limit(5).all()
-    
+    records = Strain.query.filter(Strain.medical.ilike(f"%{medical}%", escape="/")).order_by(func.random()).limit(5).all()  
+   
     medicals = []
 
     for med in records:
@@ -109,7 +96,6 @@ def get_medical(medical):
             "flavor": med.flavors, 
             "negative": med.negative
         })
-
     return jsonify(medicals)
 
 @strain_routes.route('/positive/<positive>', methods=['GET'])
@@ -131,12 +117,10 @@ def get_positve(positive):
             "flavor": pos.flavors, 
             "negative": pos.negative
         })
-
     return jsonify(positives)
 
 @strain_routes.route('/flavors/<flavors>', methods=['GET'])
 def get_flavors(flavors):
-
     """User can query Strains DB based on flavor preferences."""
 
     records = Strain.query.filter(Strain.flavors.ilike(f"%{flavors}%", escape="/")).order_by(func.random()).limit(5).all()
@@ -153,5 +137,4 @@ def get_flavors(flavors):
             "flavor": tas.flavors, 
             "negative": tas.negative
         })
-
     return jsonify(tastes)

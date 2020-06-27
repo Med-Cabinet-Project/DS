d5bd5eb79fd5b8da84af23d1741558a53810dfe6
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-27 17:46:45 -0400

    Update practice.py - create cannabis[score] column that adds 0.5 if a string is in the medical or postive columns

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import numpy as np
+import re
 import os
 
 cannabis = pd.read_csv(os.path.join(os.path.dirname(__file__), "web_app", "stats_model", "cannabis.csv"))
@@ -9,23 +10,19 @@ cannabis['medical'] = cannabis['medical'].astype(str)
 cannabis['positive'] = cannabis['positive'].astype(str)
 
 
-symptom_1 = str(input("what is the first symptom: "))
-sypmtom_2 = str(input("what is the second symptom: "))
-positve_1 = str(input("what is a positve effect you'd like to experience: "))
-positive_2 = str(input("what is a another positve effect you'd like to experience: "))
+symptom_1 = input("what is the first symptom: ")#"pain|"
+cannabis['score1'] = np.where(cannabis['medical'].str.contains((symptom_1), flags=re.IGNORECASE, regex=True), +0.5, 0)
 
-def sum_row(df): 
-    for row in df:
-        if row['medical'].str.contains('symptom_1'):
-            df['score'] += 0.5
-        if row['medical'].str.contains('symptom_1'):
-            df['score'] += 0.5
-        if row['positive'].str.contains('positive'):
-            df['score'] += 0.5
-        if row['positive'].str.contains('positive'):
-            df['score'] += 0.5
-        else:
-            0
-    return df['score']
+symptom_2 = input("what is the second symptom: ")#"headache|" 
+cannabis['score2'] = np.where(cannabis['medical'].str.contains((symptom_2), flags=re.IGNORECASE, regex=True), +0.5, 0)
 
-cannabis.apply(sum_row)
+positive_1 = input("what is a positve effect you'd like to experience: ")#"euphoria" 
+cannabis['score3'] = np.where(cannabis['positive'].str.contains((positive_1), flags=re.IGNORECASE, regex=True), +0.5, 0)
+
+positive_2 = input("what is a another positve effect you'd like to experience: ")#"relaxed" 
+cannabis['score4'] = np.where(cannabis['positive'].str.contains((positive_2), flags=re.IGNORECASE, regex=True), +0.5, 0)
+
+cannabis['score'] = cannabis['score1']+cannabis['score2']+cannabis['score3']+cannabis['score4']
+
+print(cannabis['score'].to_string())
+
